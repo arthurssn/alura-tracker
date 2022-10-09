1472f55b72deeb694928a2583d2257c4941bae5e
arthurssn <arthurnovaes55@gmail.com>
2022-10-09 10:14:18 -0300

    add cronometro

---
+++ src/components/Cronometro.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <section>
+            <strong>
+                {{tempoDecorrido}}
+            </strong>
+        </section>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Cronometro',
+    props: {
+        tempoEmSegundos: {
+            type: Number,
+            required: true,
+            default: 0
+        }
+    },
+    computed: {
+        tempoDecorrido(): string {
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substring(11, 19);
+        }
+    }
+})
+
+</script>
--- src/components/FormularioControle.vue
+++ src/components/FormularioControle.vue
@@ -6,16 +6,16 @@
             </div>
             <div class="column">
                 <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <section>
-                        <strong>00:00:00</strong>
-                    </section>
-                    <button class="button">
+                    <v-cronometro :tempoEmSegundos="tempoEmSegundos"></v-cronometro>
+
+                    <button class="button" @click="iniciar()">
                         <span class="icon">
                             <i class="fas fa-play"></i>
                         </span>
                         <span>Play</span>
                     </button>
-                    <button class="button">
+
+                    <button class="button" @click="finalizar()">
                         <span class="icon">
                             <i class="fas fa-stop"></i>
                         </span>
@@ -29,6 +29,31 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Cronometro from './Cronometro.vue';
 export default defineComponent({
+    data() {
+        return {
+            tempoEmSegundos: 0,
+            cronometro: 0
+        }
+    },
+    components: {
+        'v-cronometro': Cronometro
+    },
+
+    methods: {
+        iniciar(): void {
+            this.cronometro = setInterval(() => {
+                this.tempoEmSegundos += 1;
+                console.log(this.tempoEmSegundos)
+            }, 1000)
+        },
+        finalizar(): void {
+            clearInterval(this.cronometro);
+        },
+        // reiniciar(): void {
+
+        // },
+    }
 })
 </script>
