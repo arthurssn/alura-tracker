93be7c211420238183539f699390d6ad657ca89e
arthurssn <arthurnovaes55@gmail.com>
2022-10-09 10:32:09 -0300

    refatorando formulario

--- src/components/FormularioControle.vue
+++ src/components/FormularioControle.vue
@@ -1,27 +1,11 @@
 <template>
     <div class="box">
         <div class="columns">
-            <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+            <div class="column is-7" role="form" aria-label="Formulário para criação de uma nova tarefa">
                 <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
             </div>
             <div class="column">
-                <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <v-cronometro :tempoEmSegundos="tempoEmSegundos"></v-cronometro>
-
-                    <button class="button" @click="iniciar()">
-                        <span class="icon">
-                            <i class="fas fa-play"></i>
-                        </span>
-                        <span>Play</span>
-                    </button>
-
-                    <button class="button" @click="finalizar()">
-                        <span class="icon">
-                            <i class="fas fa-stop"></i>
-                        </span>
-                        <span>Stop</span>
-                    </button>
-                </div>
+                <meu-temporizador></meu-temporizador>
             </div>
         </div>
     </div>
@@ -29,31 +13,10 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Cronometro from './Cronometro.vue';
+import Temporizador from './Temporizador.vue'
 export default defineComponent({
-    data() {
-        return {
-            tempoEmSegundos: 0,
-            cronometro: 0
-        }
-    },
     components: {
-        'v-cronometro': Cronometro
+        'meu-temporizador': Temporizador
     },
-
-    methods: {
-        iniciar(): void {
-            this.cronometro = setInterval(() => {
-                this.tempoEmSegundos += 1;
-                console.log(this.tempoEmSegundos)
-            }, 1000)
-        },
-        finalizar(): void {
-            clearInterval(this.cronometro);
-        },
-        // reiniciar(): void {
-
-        // },
-    }
 })
 </script>
